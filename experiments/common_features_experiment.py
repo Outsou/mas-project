dac6e7714920de2e767eaec3ee156aea5b601a66
Hantula Otto A <ottohant@cs.helsinki.fi>
2017-09-11 14:16:51 +0300

    Added python files for different experiments.

---
+++ experiments/common_features_experiment.py
@@ -0,0 +1,11 @@
+"""Experiment with the active agent being able to observe 1-5 of the features all other agents observe"""
+from experiments.multi_learner_base import run_experiment
+
+if __name__ == "__main__":
+    params = None # default params
+    loop = ('common_features', list(range(1, 6))) # range from 1 to 5
+
+    num_of_simulations = 50
+    num_of_steps = 1000
+
+    run_experiment(params, loop, num_of_simulations, num_of_steps)
