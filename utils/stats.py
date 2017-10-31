159d6bc1cc1ee6a1fa29a2b7b11072264f6be6b7
Outsou <ottohant@cs.helsinki.fi>
2017-10-31 14:02:36 +0200

    Class stats.

--- utils/stats.py
+++ utils/stats.py
@@ -387,6 +387,7 @@ def get_step_top_values(collab_evals, pref_lists, collab_steps):
     top = {'all': {'eval': [[] for x in range(collab_steps)],
                    'val': [[] for x in range(collab_steps)],
                    'nov': [[] for x in range(collab_steps)]}}
+    aesthetic_top = {}
 
     for k in top_k:
         top[str(k)] = {'eval': [[] for x in range(collab_steps)],
@@ -404,6 +405,18 @@ def get_step_top_values(collab_evals, pref_lists, collab_steps):
             val = collab_evals[collab_art][creator][1]['value']
             nov = collab_evals[collab_art][creator][1]['novelty']
             pref_list = pref_lists[creator][idx]
+            aest = collab_evals[collab_art][creator][1]['aesthetic']
+
+
+            if aest not in aesthetic_top:
+                aesthetic_top[aest] = {}
+                for k in top_k:
+                    aesthetic_top[aest][str(k)] = {'eval': [],
+                                                   'val': [],
+                                                   'nov': []}
+                aesthetic_top[aest]['all'] = {'eval': [],
+                                              'val': [],
+                                              'nov': []}
 
             for k in top_k:
                 if other in pref_list[:k]:
@@ -411,10 +424,18 @@ def get_step_top_values(collab_evals, pref_lists, collab_steps):
                     top[str(k)]['val'][idx].append(val)
                     top[str(k)]['nov'][idx].append(nov)
 
+                    aesthetic_top[aest][str(k)]['eval'].append(eval)
+                    aesthetic_top[aest][str(k)]['val'].append(val)
+                    aesthetic_top[aest][str(k)]['nov'].append(nov)
+
             top['all']['eval'][idx].append(eval)
             top['all']['val'][idx].append(val)
             top['all']['nov'][idx].append(nov)
-    return top
+
+            aesthetic_top[aest]['all']['eval'].append(eval)
+            aesthetic_top[aest]['all']['val'].append(val)
+            aesthetic_top[aest]['all']['nov'].append(nov)
+    return top, aesthetic_top
 
 
 def append_top_dictionaries(dict1, dict2):
@@ -431,6 +452,20 @@ def append_top_dictionaries(dict1, dict2):
                 res_dict[key][sub_key].append(dict1[key][sub_key][i] + dict2[key][sub_key][i])
     return res_dict
 
+
+def append_aest_top_dicts(dict1, dict2):
+    if len(list(dict1.keys())) == 0:
+        return dict2
+
+    res_dict = {}
+    for aest in dict1.keys():
+        res_dict[aest] = {}
+        for k in dict1[aest].keys():
+            res_dict[aest][k] = {}
+            for key in dict1[aest][k].keys():
+                res_dict[aest][k][key] = dict1[aest][k][key] + dict2[aest][k][key]
+
+    return res_dict
 
 def calculate_top_dict_means(top_dict):
     """Calculates stepwise means and overall means."""
@@ -450,6 +485,17 @@ def calculate_top_dict_means(top_dict):
     return res_dict
 
 
+def calculate_aest_top_means(aest_dict):
+    mean_dict = {}
+    for aest in aest_dict.keys():
+        mean_dict[aest] = {}
+        for k in aest_dict[aest].keys():
+            mean_dict[aest][k] = {}
+            for key in aest_dict[aest][k].keys():
+                mean_dict[aest][k][key] = np.mean(aest_dict[aest][k][key])
+    return mean_dict
+
+
 def analyze_collab_evals(dirs):
     pickle_name = 'collab_evals.pkl'
     collab_eval_stats = common_society_analysis(dirs, pickle_name)
@@ -463,11 +509,8 @@ def analyze_collab_evals(dirs):
     collab_ratios = []
 
     aesthetic_pair_vals = {}
-    first_choice_vals = {'eval': [],
-                         'val': [],
-                         'nov': []}
     top_pick_stats = {}
-    collab_successes = 0
+    aesthetic_top_stats = {}
     step_successes = np.zeros(collab_iters)
     for dir in dirs:
         # Load pickles
@@ -481,12 +524,11 @@ def analyze_collab_evals(dirs):
         step_successes += collab_success_per_step(list(collab_evals.keys()), collab_iters)
 
         # Calculate per step top pick values
-        top_pick_stats = append_top_dictionaries(top_pick_stats,
-                                                 get_step_top_values(collab_evals, pref_lists, collab_iters))
+        top_picks, aesthetic_tops = get_step_top_values(collab_evals, pref_lists, collab_iters)
+        top_pick_stats = append_top_dictionaries(top_pick_stats, top_picks)
+        aesthetic_top_stats = append_aest_top_dicts(aesthetic_top_stats, aesthetic_tops)
 
         for collab_art in collab_arts:
-            step = int(collab_art[:5])
-            collab_step = int(step / 2 - 1)
             creators = collab_evals[collab_art]['creator'].split(' - ')
 
             # Calculate averages for aesthetic pairs
@@ -522,8 +564,10 @@ def analyze_collab_evals(dirs):
                                                       'count': vals['count']}
 
     top_pick_means = calculate_top_dict_means(top_pick_stats)
+    aesthetic_top_pick_means = calculate_aest_top_means(aesthetic_top_stats)
 
     collab_eval_stats['top_pick_stats'] = top_pick_means
+    collab_eval_stats['aest_top_pick_stats'] = aesthetic_top_pick_means
     collab_eval_stats['step_successes'] = step_successes / len(dirs)
     collab_eval_stats['num_of_agents'] = agents
 
@@ -555,6 +599,7 @@ def common_agent_analysis(dirs, pkl_name):
     stat_dict['avg_val'] = sum(vals) / len(vals)
     return stat_dict
 
+
 def analyze_collab_arts(dirs):
     pkl_name = 'collab_arts.pkl'
     collab_art_stats = common_agent_analysis(dirs, pkl_name)
@@ -572,6 +617,7 @@ def analyze_collab_arts(dirs):
             aest = general_info['aesthetic']
             if aest not in aesthetic_stats:
                 aesthetic_stats[aest] = {}
+
             for i in range(len(collab_arts_dict['fb'])):
                 caest = collab_arts_dict['caest'][i]
                 if caest not in aesthetic_stats[aest]:
@@ -620,6 +666,49 @@ def analyze_collab_arts(dirs):
 
 def analyze_own_arts(dirs):
     own_art_stats = common_agent_analysis(dirs, 'own_arts.pkl')
+
+    first_dir = os.path.split(dirs[0])[1]
+    iters = int(int(re.findall(r'i\d+', first_dir)[0][1:]) / 2)
+
+    vals = [[] for _ in range(iters)]
+    novs = [[] for _ in range(iters)]
+    aest_stats = {}
+
+    for dir in dirs:
+        sub_dirs = get_dirs_in_dir(dir)
+        for sub_dir in sub_dirs:
+            pkl = os.path.join(sub_dir, 'own_arts.pkl')
+            own_arts = pickle.load(open(pkl, 'rb'))
+            general_info = pickle.load(open(os.path.join(sub_dir, 'general_info.pkl'), 'rb'))
+            aest = general_info['aesthetic']
+
+            if aest not in aest_stats:
+                aest_stats[aest] = {'evals': [],
+                                    'vals': [],
+                                    'novs': []}
+
+            aest_stats[aest]['evals'] += own_arts['eval']
+            aest_stats[aest]['vals'] += own_arts['val']
+            aest_stats[aest]['novs'] += own_arts['nov']
+
+            for i in range(iters):
+                vals[i].append(own_arts['val'][i])
+                novs[i].append(own_arts['nov'][i])
+
+    for i in range(iters):
+        vals[i] = np.mean(vals[i])
+        novs[i] = np.mean(novs[i])
+
+    aest_means = {}
+    for aest in aest_stats.keys():
+        aest_means[aest] = {}
+        aest_means[aest]['eval'] = np.mean(aest_stats[aest]['evals'])
+        aest_means[aest]['val'] = np.mean(aest_stats[aest]['vals'])
+        aest_means[aest]['nov'] = np.mean(aest_stats[aest]['novs'])
+
+    own_art_stats['step_vals'] = vals
+    own_art_stats['step_novs'] = novs
+    own_art_stats['aest_means'] = aest_means
     return own_art_stats
 
 def analyze_model_dir(path):
@@ -738,10 +827,25 @@ def create_val_nov_plot(step_pick_stat_dict):
         plt.savefig('{}_collab.png'.format(val))
         plt.close()
 
+def create_solo_val_nov_plots(step_vals_novs_solo):
+    first_key = list(step_vals_novs_solo.keys())[0]
+    x = list(range(2, len(step_vals_novs_solo[first_key]['vals']) * 2 + 1, 2))
 
-def analyze_collab_gp_runs(path, decimals=3):
+    for val in ['vals', 'novs']:
+        for model in sorted(step_vals_novs_solo.keys()):
+            plt.plot(x, step_vals_novs_solo[model][val], label=model)
+        plt.xlabel('step')
+        plt.ylabel(val)
+        plt.legend()
+        plt.savefig('{}_solo.png'.format(val))
+        plt.close()
+
+
+def analyze_collab_gp_runs(path, decimals=3, exclude=None):
     """The main function to call when analyzing runs."""
     sns.set(color_codes=True)
+    sns.set_context("paper")
+    sns.set_style("white")
     model_dirs = sorted(get_dirs_in_dir(path))
     format_s = '%.{}f'.format(decimals)
 
@@ -762,16 +866,52 @@ def analyze_collab_gp_runs(path, decimals=3):
             ['First choice collab evaluation'],
             ['First choice collab value'],
             ['First choice collab novelty']]
+    aest_rows = {}
+    aest_first_choice_rows = {}
 
     models = ['']
     cumulative_successes = {}
     step_success_ratios = {}
     step_pick_stat_dict = {}
+    step_vals_novs_solo = {}
 
     for model_dir in model_dirs:
         model = os.path.split(model_dir)[1]
+
+        if model in exclude:
+            continue
+
         models.append(model)
         collab_eval_stats, collab_art_stats, own_art_stats, ind_eval_stats = analyze_model_dir(model_dir)
+
+        for aest in collab_eval_stats['aest_top_pick_stats']:
+            if aest not in aest_rows:
+                aest_rows[aest] = [['{} own collab eval'.format(aest)],
+                                   ['{} own solo eval'.format(aest)],
+                                   ['{} own collab val'.format(aest)],
+                                   ['{} own solo val'.format(aest)],
+                                   ['{} own collab nov'.format(aest)],
+                                   ['{} own solo nov'.format(aest)]]
+            col_vals = [collab_eval_stats['aest_top_pick_stats'][aest]['all']['eval'],
+                        own_art_stats['aest_means'][aest]['eval'],
+                        collab_eval_stats['aest_top_pick_stats'][aest]['all']['val'],
+                        own_art_stats['aest_means'][aest]['val'],
+                        collab_eval_stats['aest_top_pick_stats'][aest]['all']['nov'],
+                        own_art_stats['aest_means'][aest]['nov']]
+            for i in range(len(col_vals)):
+                col_vals = [format_s % val if type(val) in [float, np.float64] else val for val in col_vals]
+                aest_rows[aest][i].append(col_vals[i])
+
+            if aest not in aest_first_choice_rows:
+                aest_first_choice_rows[aest] = [['{} first choice eval'.format(aest)],
+                                                ['{} first choice val'.format(aest)],
+                                                ['{} first choice nov'.format(aest)]]
+            col_vals = [collab_eval_stats['aest_top_pick_stats'][aest]['1']['eval'],
+                        collab_eval_stats['aest_top_pick_stats'][aest]['1']['val'],
+                        collab_eval_stats['aest_top_pick_stats'][aest]['1']['nov']]
+            for i in range(len(col_vals)):
+                col_vals = [format_s % val if type(val) in [float, np.float64] else val for val in col_vals]
+                aest_first_choice_rows[aest][i].append(col_vals[i])
 
         conf_int = collab_eval_stats['success_ratio']['conf_int']
         conf_int = (format_s % conf_int[0], format_s % conf_int[1])
@@ -820,7 +960,16 @@ def analyze_collab_gp_runs(path, decimals=3):
 
         step_pick_stat_dict[model] = collab_eval_stats['top_pick_stats']
 
+        step_vals_novs_solo[model] = {}
+        step_vals_novs_solo[model]['vals'] = own_art_stats['step_vals']
+        step_vals_novs_solo[model]['novs'] = own_art_stats['step_novs']
+
     # Print main table
+    for aest in sorted(aest_rows.keys()):
+        for row in aest_rows[aest]:
+            rows.append(row)
+        for row in aest_first_choice_rows[aest]:
+            rows.append(row)
     print(tabulate(rows, headers=models))
 
     # Make cumulative success ratio plot
@@ -829,5 +978,8 @@ def analyze_collab_gp_runs(path, decimals=3):
     # Make non-cumulative success ratio plot
     make_success_ratio_plot(step_success_ratios, 'success_ratios.png')
 
-    # Make value and novelty plots
+    # Make value and novelty collab plots
     create_val_nov_plot(step_pick_stat_dict)
+
+    # Make value and novelty solo plots
+    create_solo_val_nov_plots(step_vals_novs_solo)
